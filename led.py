715caf92ebdcfb9ce8cebc1e4bad266e1611a24d
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-04-13 14:32:25 -0400

    Add files via upload

--- led.py
+++ led.py
@@ -3,10 +3,99 @@
 from flask import Flask, render_template
 import datetime
 import RPi.GPIO as GPIO
+import sys
 
 redPin = 21
 greenPin = 20
 bluePin = 16
+
+def blink(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+def turnOff(pin):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+
+def redOn():
+    blink(redPin)
+
+def greenOn():
+    blink(greenPin)
+
+def blueOn():
+    blink(bluePin)
+
+def yellowOn():
+    blink(redPin)
+    blink(greenPin)
+
+def cyanOn():
+    blink(greenPin)
+    blink(bluePin)
+
+def magentaOn():
+    blink(redPin)
+    blink(bluePin)
+
+def whiteOn():
+    blink(redPin)
+    blink(greenPin)
+    blink(bluePin)
+
+def redOff():
+    turnOff(redPin)
+
+def greenOff():
+    turnOff(greenPin)
+
+def blueOff():
+    turnOff(bluePin)
+
+def yellowOff():
+    turnOff(redPin)
+    turnOff(greenPin)
+
+def cyanOff():
+    turnOff(greenPin)
+    turnOff(bluePin)
+
+def magentaOff():
+    turnOff(redPin)
+    turnOff(bluePin)
+
+def whiteOff():
+    turnOff(redPin)
+    turnOff(greenPin)
+    turnOff(bluePin)
+
+def turnOn(status, color, intensity):
+    if status == "off":
+        whiteOff()
+    if color == "red":
+        redOn()
+    elif color == "green":
+        greenOn()
+    elif color == "blue":
+        blueOn()
+    elif color == "yellow":
+        yellowOn()
+    elif color == "cyan":
+        cyanOn()
+    elif color == "magenta":
+        magentaOn()
+    elif color == "white":
+        whiteOn()
+
+
+def main():
+    while True:
+        status = input("on or off?: ")
+        color = input("color?: ")
+        intensity = input("intensity?: ")
+        turnOn(status, color, intensity)
 
 # app = Flask(__name__)
 #
