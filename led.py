ea55d3fe76eaf3afdacbb37188072181f76c8f41
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-04-13 14:09:23 -0400

    Add files via upload

--- led.py
+++ led.py
@@ -1,1 +1,18 @@
 #ECE 4564
+
+from flask import Flask, render_template
+import datetime
+import RPi.GPIO as GPIO
+
+redPin = 21
+greenPin = 20
+bluePin = 16
+
+# app = Flask(__name__)
+#
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+#
+# if __name__ == "__main__":
+#     app.run(host='0.0.0.0', port=80, debug=True)
--- services.py
+++ services.py
@@ -1,1 +1,16 @@
 #ECE 4564
+
+# figure out how to get a canvas token
+
+# figure out PWN stuff on RPi GPIO pins
+    # on/off
+    # color - red/green/blue/magenta/cyan/yellow/white
+    # brightness
+
+# get flask services set up
+import requests as requests
+#10.0.0.101/24
+
+ip_addr = "10.0.0.101"
+port = "24"
+r = requests.get("http://" + ip_addr + ":" + port + "/LED?status=on&color=red&intensity=50")
